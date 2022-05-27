e0c52676b614e78b4e16c85eaef4024e5b95441c
Amitdhal007 <amit0162.be21@chitkara.edu.in>
2022-05-27 19:27:15 +0530

    Function file added

---
+++ function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void myfunction(string name ,int age){
+    cout<<name<<" Bhai"<<" of Age "<<age<<endl;
+    }
+
+int main(){
+    myfunction("Akshay",23);
+    // myfunction();
+    return 0;
+}
+
